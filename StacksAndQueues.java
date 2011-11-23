380569586da064a685701b80161033a1f0a66f2c
Chris Arriola <arriolac279@gmail.com>
2011-11-23 08:03:58 -0800

    Tested Stack and Queue

--- StacksAndQueues.java
+++ StacksAndQueues.java
@@ -10,5 +10,22 @@ public class StacksAndQueues {
      */
     public static void main(String[] args) {
 
+        // Test Stack
+        Stack<Integer> s = new Stack<Integer>();
+        s.push(10);
+        s.push(20);
+
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+
+        // Test Queue
+        Queue<Integer> q = new Queue<Integer>();
+        q.enqueue(10);
+        q.enqueue(20);
+
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
     }
 }
