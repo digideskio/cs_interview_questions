495d53650322fa2270f4fbb631639bb235c4b432
Chris Arriola <arriolac279@gmail.com>
2011-11-28 10:56:01 -0800

    Added test for ThreeStacksOneArray1 and ThreeStacksOneArray2

--- src/main/StacksAndQueues.java
+++ src/main/StacksAndQueues.java
@@ -45,5 +45,68 @@ public class StacksAndQueues {
         System.out.println(stacks.pop());
         System.out.println(stacks.pop());
         System.out.println(stacks.pop());
+
+        // Test MyQueue
+        MyQueue<Integer> myqueue = new MyQueue<Integer>();
+        myqueue.enqueue(1); 
+        myqueue.enqueue(2);
+        myqueue.enqueue(3);
+
+        System.out.println(myqueue.dequeue());
+
+        myqueue.enqueue(4);
+        myqueue.enqueue(5);
+
+        System.out.println(myqueue.dequeue());
+        System.out.println(myqueue.dequeue());
+        System.out.println(myqueue.dequeue());
+        System.out.println(myqueue.dequeue());
+
+        // Test ThreeStacksOneArray1
+        ThreeStacksOneArray1 threeStacks1 = new ThreeStacksOneArray1(5);
+        threeStacks1.push(0, 1); 
+        threeStacks1.push(0, 2); 
+        threeStacks1.push(0, 3); 
+      
+        threeStacks1.push(1, 1); 
+        threeStacks1.push(1, 2); 
+        threeStacks1.push(1, 3); 
+        threeStacks1.push(1, 4); 
+        threeStacks1.push(1, 5); 
+        threeStacks1.push(1, 6);  // shouldn't work
+
+        threeStacks1.push(2, 1); 
+        threeStacks1.push(2, 2); 
+        threeStacks1.push(2, 3); 
+
+        System.out.println("stack0 top: " + threeStacks1.peek(0));
+        System.out.println("stack1 top: " + threeStacks1.peek(1));
+        System.out.println("stack2 top: " + threeStacks1.peek(2));
+
+        // Test ThreeStacksOneArray2
+        ThreeStacksOneArray2 threeStacks2 = new ThreeStacksOneArray2(5);
+        threeStacks2.push(0, 1); 
+        threeStacks2.push(0, 3); 
+        threeStacks2.push(0, 2); 
+        threeStacks2.push(0, 4); 
+        threeStacks2.push(0, 5); 
+        threeStacks2.push(0, 6); 
+        threeStacks2.pop(0);
+
+        threeStacks2.push(1, 1); 
+        threeStacks2.push(1, 2); 
+        threeStacks2.push(1, 3); 
+        threeStacks2.push(1, 4); 
+        threeStacks2.push(1, 5); 
+        threeStacks2.push(1, 6);  // shouldn't work
+
+        threeStacks2.push(2, 1); 
+        threeStacks2.push(2, 2); 
+        threeStacks2.push(2, 3); 
+
+        System.out.println("stack0 top: " + threeStacks2.peek(0));
+        System.out.println("stack1 top: " + threeStacks2.peek(1));
+        System.out.println("stack2 top: " + threeStacks2.peek(2));
+
     }
 }
