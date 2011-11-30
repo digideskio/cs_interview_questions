bc3baaf7e6af12d2f4999996f0708cb345666276
Chris Arriola <arriolac279@gmail.com>
2011-11-29 22:48:28 -0800

    Added Tree questions.

--- src/main/tree/Tree.java
+++ src/main/tree/Tree.java
@@ -6,40 +6,17 @@ import java.util.ArrayList;
  * Tree datastructure implementation.
  */
 public class Tree<V> {
-    V value;
-    ArrayList<Tree<V>> children;
+    public V value;
+    public Tree<V> left = null;
+    public Tree<V> right = null;
 
-    public Tree(V val, Tree<V>... children) {
+    public Tree(V val) {
         this.value = val;
-        this.children = new ArrayList<Tree<V>>();
-
-        for (Tree<V> child : children) {
-            this.children.add(child);
-        }
     }
 
-    public void setValue(V val) {
-        value = val;
-    }
-
-    public V getValue() {
-        return value;
-    }
-
-    public boolean isLeaf() {
-        return (children.isEmpty() ? true : false);
-    }
-
-    public Tree<V> child(int index) {
-
-        if (index > children.size()-1) {
-            return null;
-        }
-
-        return children.get(index);
-    }
-
-    public int numberOfChildren() {
-        return children.size();
+    public Tree(V val, Tree<V> left, Tree<V> right) {
+        this.value = val;
+        this.left = left;
+        this.right = right;
     }
 }
